dc7d09a081871b90005c64fcf71e07a9ff55bdc4
Adam <adamo.b1997@gmail.com>
2021-02-07 14:55:56 +0100

    Add popularity filtering

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+from os import error
+import pandas
 
+def loadFile(filename):
+    try:
+        return pandas.read_csv(filename)
+    except Exception as err:
+        print(err)
+        return None
+
+
+def filterByPopularity(filename, percentage, lessThan = False):
+    data = loadFile(filename)
+    try:
+        if lessThan:
+            data.drop(data[data.popularity > percentage].index, inplace=True)
+        else:
+            data.drop(data[data.popularity < percentage].index, inplace=True)
+    except Exception as err:
+        print(err)
+
+    return data
+
+
+def saveToNewFile(data, filename = 'out.csv'):
+    try:
+        data.to_csv(filename)
+    except Exception as err:
+        print(err)
+
+
+filtered = filterByPopularity('data_by_artist.csv', 90)
+saveToNewFile(filtered)
