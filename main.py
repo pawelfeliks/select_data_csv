dd4e7f01188ed2d1b872451fbf085fbbb2a9c129
pawelf444 <76951330+pawelf444@users.noreply.github.com>
2021-02-09 15:49:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import csv
 
+
+def open_file(path):
+
+    try:
+        f = open(path, "r", encoding="utf-8")
+        for line in f:
+            print(line, end="")
+            
+    except:
+        print("Error or no such file")
+
+
+
+def open_csv(plik):
+    
+    try:
+        
+        with open(plik, 'r', encoding="utf-8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row)
+                
+    except:
+        print("Error or no such file")
+
+ 
+
+filecsv = "E:\Studia\Programowanie\Python\plik_csv.csv"
+open_csv(filecsv) 
