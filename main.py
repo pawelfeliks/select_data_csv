2b3e53052e28e1f1b2a0fb86e3d7cfff473846d5
pawelf444 <76951330+pawelf444@users.noreply.github.com>
2021-02-11 14:32:25 +0100

    ch2

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import pandas
 
 def open_csv(filename): 
     try:
-        return pandas.read_csv(filename)
-                
+        return pandas.read_csv(filename)             
     except Exception as err:
         print(err)
         return None
-
 def filter_by_pop(file):
     try:
         file.drop(file[file['popularity']<65].index, inplace=True) #usuwa wszystko ponizej 65
-        
     except Exception as err:
         print(err)
 
@@ -23,4 +20,3 @@ filecsv = "data_by_artist.csv"
 file = open_csv(filecsv) 
 filter_by_pop(file)
 save_csv(file)
-print(file)
